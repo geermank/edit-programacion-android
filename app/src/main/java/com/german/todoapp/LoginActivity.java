145d9ae036094ecf590db24aad1284eb6a59ea55
German Krajcar <german.krajcar@mercadolibre.com>
2020-08-18 22:28:21 -0300

    First commit

---
+++ app/src/main/java/com/german/todoapp/LoginActivity.java
@@ -0,0 +1,36 @@
+package com.german.todoapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class LoginActivity extends AppCompatActivity implements View.OnClickListener {
+
+    // Estas variables heredan de View
+    private EditText etEmail;
+    private EditText etPassword;
+    private Button btnLogin;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        etEmail = findViewById(R.id.et_email);
+        etPassword = findViewById(R.id.et_password);
+        btnLogin = findViewById(R.id.btn_login);
+
+        btnLogin.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        if (view == btnLogin) {
+            Toast.makeText(this, "Se hizo click", Toast.LENGTH_SHORT).show();
+        }
+    }
+}
