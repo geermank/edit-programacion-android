cd0c18cdc17fa320c3d8ff8ef9c8c45f3154f7f4
German Krajcar <german.krajcar@mercadolibre.com>
2020-08-24 21:42:48 -0300

    Clase 2 - UI properties, RelativeLayout, Intent

---
+++ app/src/main/java/com/german/todoapp/Constants.java
@@ -0,0 +1,6 @@
+package com.german.todoapp;
+
+public class Constants {
+    public static final String USER_EMAIL = "user_email";
+    public static final String USER_PASSWORD = "user_password";
+}
--- app/src/main/java/com/german/todoapp/LoginActivity.java
+++ app/src/main/java/com/german/todoapp/LoginActivity.java
@@ -2,10 +2,12 @@ package com.german.todoapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class LoginActivity extends AppCompatActivity implements View.OnClickListener {
@@ -14,6 +16,7 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
     private EditText etEmail;
     private EditText etPassword;
     private Button btnLogin;
+    private TextView tvForgotPassword;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,14 +26,34 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
         etEmail = findViewById(R.id.et_email);
         etPassword = findViewById(R.id.et_password);
         btnLogin = findViewById(R.id.btn_login);
+        tvForgotPassword = findViewById(R.id.tv_forgot_password);
 
         btnLogin.setOnClickListener(this);
+
+        // clases anonimas
+        tvForgotPassword.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(LoginActivity.this, "Olvide mi contraseña", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 
     @Override
     public void onClick(View view) {
         if (view == btnLogin) {
-            Toast.makeText(this, "Se hizo click", Toast.LENGTH_SHORT).show();
+            String email = etEmail.getText().toString();
+            String password = etPassword.getText().toString();
+            // validaciones al email y al password
+            startMainActivity(email, password);
         }
+    }
+
+    private void startMainActivity(String email, String password) {
+        Intent mainIntent = new Intent(this, MainActivity.class);
+        mainIntent.putExtra(Constants.USER_EMAIL, email);
+        mainIntent.putExtra(Constants.USER_PASSWORD, password);
+        startActivity(mainIntent);
+        finish();
     }
 }
